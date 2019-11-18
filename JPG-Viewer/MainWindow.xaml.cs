ee8d58eb3e98e8319a4ec6686b54732de1766a9a
ilyavovchenko431@gmail.com <ilyavovchenko431@gmail.com>
2019-11-18 18:19:16 +1000

    Playing with the project

--- JPG-Viewer/MainWindow.xaml.cs
+++ JPG-Viewer/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
+using System.Windows.Forms;
 
 namespace JPG_Viewer
 {
@@ -20,9 +22,51 @@ namespace JPG_Viewer
     /// </summary>
     public partial class MainWindow : Window
     {
+        public List<string> FoundImages { get; set; }
+        public List<string> FoundPaths { get; set; }
         public MainWindow()
         {
+            FolderBrowserDialog selectedFolderDialog = new FolderBrowserDialog();
+            selectedFolderDialog.ShowDialog();
+            FoundImages = FindJPEGInDirectory(selectedFolderDialog.SelectedPath);
+            FoundPaths = FindAllPathsInDirectory(selectedFolderDialog.SelectedPath);
             InitializeComponent();
+        }
+
+        public static List<string> FindJPEGInDirectory(string dir)
+        {
+            if (string.IsNullOrWhiteSpace(dir))
+                return null;
+            List<string> JPEGPaths = new List<string>();
+            DirectoryInfo directory = new DirectoryInfo(dir);
+            try
+            {
+                foreach (FileInfo file in directory.GetFiles("*", SearchOption.AllDirectories))
+                {
+                    if (file.Extension.Contains("jpg") ||
+                       file.Extension.Contains("jpeg"))
+                        JPEGPaths.Add(file.FullName);
+                }
+            }
+            catch (Exception ex) { System.Windows.MessageBox.Show(ex.Message); }
+            return JPEGPaths;
+
+        }
+        public static List<string> FindAllPathsInDirectory(string dir)
+        {
+            if (string.IsNullOrWhiteSpace(dir))
+                return null;
+            List<string> Dirs = new List<string>();
+            DirectoryInfo directory = new DirectoryInfo(dir);
+            try
+            {
+                foreach(var d in directory.GetDirectories("*", SearchOption.AllDirectories).ToList())
+                {
+                    Dirs.Add(d.Name);
+                }
+            }
+            catch (Exception ex) { System.Windows.MessageBox.Show(ex.Message); }
+            return Dirs;
         }
     }
 }
