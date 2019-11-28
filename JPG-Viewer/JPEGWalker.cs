72355a8024b839b0096a4652917fdc24b835d26d
ilyavovchenko431@gmail.com <ilyavovchenko431@gmail.com>
2019-11-28 23:17:49 +1000

    trying to work with Exif

--- JPG-Viewer/JPEGWalker.cs
+++ JPG-Viewer/JPEGWalker.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace JPG_Viewer
 {
@@ -48,24 +49,33 @@ namespace JPG_Viewer
         public string ReadExifInFile(string path)
         {
             // Каждый сектор заголовка JPEG состоит из
-            //      1. маркера (0xFFD8 - тип файла - JPEG)
-            //      2. ДЛИНЫ СЕКТОРА (два байта)
+            //      1. маркера (0xFFE1 - Exif)
+            //      2. длины сектора (два байта) + 2б длина маркера
             //      3. самих данных
             // Надо найти спецификацию JPEG, наименования маркеров и сделать чтение по этим маркерам
             string kindaExif = "";
-            int exifMarkerIndex = 4;
-            byte[] searchedExifLength = new byte[2];
+            byte[] searchExifBuffer = { 0x00, 0x00 };
             using (FileStream fs = new FileStream(path, FileMode.Open, FileAccess.Read))
             {
                 using (BinaryReader reader = new BinaryReader(fs))
                 {
                     byte swap;
-                    while(!((searchedExifLength[0] = reader.ReadByte()) == 0xFF) && !((searchedExifLength[1] = reader.ReadByte()) == 0xE1))
+                    fs.Seek(0, SeekOrigin.Begin);                              
+                    // 0xE0 - старый JFIF, 0xE1 - более новая версия стандарта Exif
+                    while (!((searchExifBuffer[0] == 0xFF) && ((searchExifBuffer[1] == 0xE0) || searchExifBuffer[1] == 0xE1))
+                        && fs.Position != fs.Length - 1)
                     {
-                        swap = searchedExifLength[0];
-                        searchedExifLength[0] = searchedExifLength[1];
-                        searchedExifLength[1] = swap;
+                        swap = searchExifBuffer[1];
+                        searchExifBuffer[1] = searchExifBuffer[0];
+                        searchExifBuffer[0] = swap;
+                        searchExifBuffer[1] = reader.ReadByte();
                     }
+                    if (fs.Position == fs.Length - 1)
+                        return "Несовместимый формат";
+                    ushort length = reader.ReadUInt16();
+                    length = (ushort)((length << 8) | (length >> 8) & 0xFF);
+                    MessageBox.Show(length.ToString());
+                    kindaExif = Encoding.ASCII.GetString(searchExifBuffer);
                 }
             }
             return kindaExif;
